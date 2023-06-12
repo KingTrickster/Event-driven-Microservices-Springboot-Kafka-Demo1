5be32b35e165854837149ef549480b6904c3c1e5
Alejandro Trejo <alextrejores@gmail.com>
2023-06-12 00:32:25 -0600

    Finished kafka producer

---
+++ order-service/src/main/java/com/trxjster/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,34 @@
+package com.trxjster.orderservice.kafka;
+
+import com.trxjster.basedomains.dto.OrderEvent;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    private final NewTopic topic;
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    public OrderProducer(NewTopic newTopic, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.topic = newTopic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(OrderEvent event){
+        LOGGER.info(String.format("Order event -> %s", event.toString()));
+
+        Message<OrderEvent> message = MessageBuilder.withPayload(event)
+                .setHeader(KafkaHeaders.TOPIC, topic.name())
+                .build();
+        kafkaTemplate.send(message);
+    }
+}
