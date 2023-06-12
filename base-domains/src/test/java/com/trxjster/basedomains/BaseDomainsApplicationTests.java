5df9c1b34bec85d860a994f43213a3725d1db175
Alejandro Trejo <alextrejores@gmail.com>
2023-06-12 00:13:40 -0600

    first commit

---
+++ base-domains/src/test/java/com/trxjster/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trxjster.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
